a502ebf7ab1628ee54f0b7558379291d32215e00
heikunKeikun <jjouhansson@hotmail.fi>
2019-04-07 00:09:06 +0300

    eating food works now

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
     - handle border collision (DONE)
     - make the snake move on itself (DONE)
     - add food on random spot (DONE)
-      - make sure that the food does not spawn on the snake
+      - make sure that the food does not spawn on the snake (DONE)
     - handle snake colliding with itself
-    - make the snake longer after eating food
-      - food reappears after eating
+    - make the snake longer after eating food (DONE)
+      - food reappears after eating (DONE)
     - game winning/losing conditions
     - score system
     - UI?
@@ -19,27 +19,41 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 ctx.font = "25px Arial";
-ctx.fillText("Press enter to start the game!", 90, 225); 
+ctx.fillText("Press [ENTER] to start the game!", 70, 225); 
+window.addEventListener('keydown', (event) => handleKeyPress(event));
 
-let snake = [{x:225,y:225}, {x:250,y:225}, {x:275,y:225}, {x:300,y:225}];
+let snake = [{x:225, y:225}, {x:250, y:225}, {x:275, y:225}, {x:300, y:225}];
 let food = {};
 let direction = 'left';
 let gameStarted = false;
+let gamePaused = false;
+var game = null;
 
 function startGame() {
-    spawnFood();
+    clear();
+    placeFood();
     drawSnake();
-    setInterval(() => move(), 400);
+    game = setInterval(() => move(), 200);
 
     document.getElementById("canvas").focus();
 }
 
-function spawnFood() {
-    const x_axis = Math.round(Math.floor(Math.random() * canvas.width) / 25) * 25;
-    const y_axis = Math.round(Math.floor(Math.random() * canvas.height) / 25) * 25;
-    food = {x:x_axis, y:y_axis};
+function pauseGame() {
+    if (gamePaused) {
+        game = setInterval(() => move(), 200);
+        gamePaused = false;
+    } else {
+        ctx.fillStyle = '#000000';
+        ctx.font = "50px Arial";
+        ctx.fillText("Game paused!", 90, 225); 
+        clearInterval(game);
+        gamePaused = true;
+    }
+}
+
+function draw() {
     drawFood();
-    console.log(food);
+    drawSnake();
 }
 
 function drawFood() {
@@ -65,7 +79,30 @@ function clear() {
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
-window.addEventListener('keydown', (event) => handleKeyPress(event));
+function placeFood() {
+    const x_axis = Math.round(Math.floor(Math.random() * (canvas.width-25)) / 25) * 25;
+    const y_axis = Math.round(Math.floor(Math.random() * (canvas.height-25)) / 25) * 25;
+    for (const trail of snake) {
+        if (x_axis === trail.x && y_axis === trail.y) {
+            console.log(`x_axis: ${x_axis}, y_axis:${y_axis}`);
+            console.log('food on snake! RESETTING!');
+            placeFood();
+            return;
+        }
+    }
+    food = {x:x_axis, y:y_axis};
+    console.log('food placed at ', food);
+    drawFood();
+}
+
+function eatFood() {
+    if (snake[0].x === food.x && snake[0].y === food.y) {
+        snake.push({x:food.x, y:food.y});
+        drawSnake();
+        console.log('nom nom nom');
+        placeFood();
+    } 
+}
 
 function move() {
     switch(direction) {
@@ -74,8 +111,7 @@ function move() {
             snake.unshift({x:snake[0].x, y:snake[0].y-25});
             snake.pop();
             clear();
-            drawSnake();
-            drawFood();
+            draw();
             break;
 
         case 'down':
@@ -83,8 +119,7 @@ function move() {
             snake.unshift({x:snake[0].x, y:snake[0].y+25});
             snake.pop();
             clear();
-            drawSnake();
-            drawFood();
+            draw();
             break;
 
         case 'left':
@@ -93,8 +128,7 @@ function move() {
             snake.unshift({x:snake[0].x-25, y:snake[0].y});
             snake.pop();
             clear();
-            drawSnake();
-            drawFood();
+            draw();
             break;
 
         case 'right':
@@ -103,17 +137,16 @@ function move() {
             snake.unshift({x:snake[0].x+25, y:snake[0].y});
             snake.pop();
             clear();
-            drawSnake();
-            drawFood();
+            draw();
             break;
 
         default:
             break;
     }
+    eatFood();
 }
 
 function handleKeyPress(event) {
-    console.log(event)
     switch(event.key) {
         case 'ArrowUp':
             if (direction === 'up' || direction === 'down') break;
@@ -141,9 +174,11 @@ function handleKeyPress(event) {
             gameStarted = true;
             break;
 
+        case ' ':
+            if (gameStarted) pauseGame();
+            break;
+
         default:
             break;
     }
-
-    console.log(snake[0])
 }
