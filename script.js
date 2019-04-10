22fcd32a10f3900a08a97d0ad3d7b26b95cfd894
heikunKeikun <jjouhansson@hotmail.fi>
2019-04-11 01:12:17 +0300

    snake now appears from the opposite side when hitting the wall. added also first version of speeding up.

--- script.js
+++ script.js
@@ -13,21 +13,21 @@
       - food reappears after eating (DONE)
     - game winning/losing conditions (DONE)
     - score system (DONE)
+    - make the snake appear on the opposite side when hitting the border (DONE)
     - make the snake faster after certain point limit?
-    - make the snake appear on the opposite side when hitting the border?
+    - save hiscore locally
     - UI?
 
     FIXES:
     - bug with the snake able to turn 180 degrees (DONE)
 */
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 ctx.font = "20px Arial";
 ctx.fillText("[ENTER] - start the game", 60, 200);
-ctx.fillText("[SPACE] - pause the game", 60, 240); 
-ctx.fillText("[UP, DOWN, LEFT, RIGHT] - steer the snake", 60, 280); 
-window.addEventListener('keydown', (event) => handleKeyPress(event));
+ctx.fillText("[SPACE] - pause the game", 60, 240);
+ctx.fillText("[UP, DOWN, LEFT, RIGHT] - steer the snake", 60, 280);
 
 let snake = null;
 let food = {};
@@ -36,59 +36,62 @@ let direction = null;
 let gameStarted = false;
 let gameOver = false;
 let gamePaused = false;
-var game = null;
+let game = null;
 let tick = 0;
 let movementAt = null;
+let interval = 250;
+
+window.addEventListener("keydown", (event) => handleKeyPress(event));
 
 function startGame() {
     snake = [{x:225, y:225}, {x:250, y:225}, {x:275, y:225}, {x:300, y:225}];
     score = 0;
     ticks = 0;
-    direction = 'left';
+    direction = "left";
     gameOver = false;
     gameStarted = true;
     clear();
     placeFood();
     drawSnake();
-    game = setInterval(() => move(), 200);
+    game = setInterval(() => move(), interval);
 
     document.getElementById("canvas").focus();
 }
 
 function pauseGame() {
     if (gamePaused) {
-        game = setInterval(() => move(), 200);
+        game = setInterval(() => move(), interval);
         gamePaused = false;
     } else {
-        ctx.fillStyle = '#000000';
+        ctx.fillStyle = "#000000";
         ctx.font = "25px Arial";
-        ctx.fillText("Game paused!", 90, 180); 
-        ctx.fillText("Press [SPACE] to continue", 90, 210); 
+        ctx.fillText("Game paused!", 90, 180);
+        ctx.fillText("Press [SPACE] to continue", 90, 210);
         clearInterval(game);
         gamePaused = true;
     }
 }
 
 function endGame() {
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = "#000000";
     ctx.font = "50px Arial";
-    ctx.fillText("Game over!", 110, 200); 
-    ctx.fillText(`Score: ${score}`, 110, 250); 
+    ctx.fillText("Game over!", 110, 200);
+    ctx.fillText(`Score: ${score}`, 110, 250);
     ctx.font = "25px Arial";
-    ctx.fillText(`Press [ENTER] to start again!`, 110, 290); 
+    ctx.fillText(`Press [ENTER] to start again!`, 110, 290);
     clearInterval(game);
     gameOver = true;
 }
 
-// i guess i'll add this here even though it's nearly impossible
+// i guess i"ll add this here even though it"s nearly impossible
 function winGame() {
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = "#000000";
     ctx.font = "50px Arial";
-    ctx.fillText("You.. won?", 110, 200); 
+    ctx.fillText("You.. won?", 110, 200);
     ctx.font = "30px Arial";
-    ctx.fillText("Oh my god..", 120, 235); 
+    ctx.fillText("Oh my god..", 120, 235);
     ctx.font = "25px Arial";
-    ctx.fillText(`Press [ENTER] to start again..`, 110, 290); 
+    ctx.fillText(`Press [ENTER] to start again..`, 110, 290);
     clearInterval(game);
     gameOver = true;
 }
@@ -104,16 +107,16 @@ function draw() {
 }
 
 function drawFood() {
-    ctx.fillStyle = '#DC143C';
-    ctx.strokestyle = '#DDA0DD';
+    ctx.fillStyle = "#DC143C";
+    ctx.strokestyle = "#DDA0DD";
     ctx.fillRect(food.x, food.y, 25, 25);
     ctx.strokeRect(food.x, food.y, 25, 25);
 }
 
 function drawSnake() {
     for (const trail of snake) {
-        ctx.fillStyle = 'lightgreen';
-        ctx.strokestyle = 'darkgreen';
+        ctx.fillStyle = "lightgreen";
+        ctx.strokestyle = "darkgreen";
         ctx.fillRect(trail.x, trail.y, 25, 25);
         ctx.strokeRect(trail.x, trail.y, 25, 25);
     }
@@ -129,7 +132,7 @@ function clear() {
 function placeFood() {
     const x_axis = Math.round(Math.floor(Math.random() * (canvas.width-25)) / 25) * 25;
     const y_axis = Math.round(Math.floor(Math.random() * (canvas.height-25)) / 25) * 25;
-    for (const trail of snake) {
+    for (const trail of snake) { // if the food is placed on the snake, don't.
         if (x_axis === trail.x && y_axis === trail.y) {
             placeFood();
             return;
@@ -146,52 +149,59 @@ function eatFood() {
         placeFood();
         score++;
         document.getElementById("score").innerText = `Score: ${score}`;
-    } 
+
+        if (score % 4 === 0 && interval > 125) {
+            console.log("speeding up")
+            clearInterval(game);
+            interval -= 25;
+            game = setInterval(() => move(), interval);
+        }
+    }
 }
 
 function move() {
+    let y = null;
+    let x = null;
     switch(direction) {
-        case 'up':
+        case "up":
+            y = snake[0].y-25;
             if (snake[0].y === 0) {
-                endGame();
-                break;
+                y = 475;
             }
-            snake.unshift({x:snake[0].x, y:snake[0].y-25});
+            snake.unshift({x:snake[0].x, y:y});
             snake.pop();
             clear();
             draw();
             break;
 
-        case 'down':
+        case "down":
+            y = snake[0].y+25;
             if (snake[0].y === 475) {
-                endGame();
-                break;
+                y = 0;
             }
-            snake.unshift({x:snake[0].x, y:snake[0].y+25});
+            snake.unshift({x:snake[0].x, y:y});
             snake.pop();
             clear();
             draw();
             break;
 
-        case 'left':
-            direction = 'left';
+        case "left":
+            x = snake[0].x-25;
             if (snake[0].x === 0) {
-                endGame();
-                break;
+                x = 475;
             }
-            snake.unshift({x:snake[0].x-25, y:snake[0].y});
+            snake.unshift({x:x, y:snake[0].y});
             snake.pop();
             clear();
             draw();
             break;
 
-        case 'right':
-            direction = 'right';
+        case "right":
+            x = snake[0].x+25;
             if (snake[0].x === 475) {
-                endGame();
-                break;
+                x = 0;
             }
-            snake.unshift({x:snake[0].x+25, y:snake[0].y});
+            snake.unshift({x:x, y:snake[0].y});
             snake.pop();
             clear();
             draw();
@@ -206,38 +216,38 @@ function move() {
 
 function handleKeyPress(event) {
     switch(event.key) {
-        case 'ArrowUp':
+        case "ArrowUp":
             if (movementAt === tick) break;
-            if (direction === 'up' || direction === 'down') break;
-            direction = 'up';
+            if (direction === "up" || direction === "down") break;
+            direction = "up";
             break;
 
-        case 'ArrowDown':
+        case "ArrowDown":
             if (movementAt === tick) break;
-            if (direction === 'up' || direction === 'down') break;
-            direction = 'down';
+            if (direction === "up" || direction === "down") break;
+            direction = "down";
             break;
 
-        case 'ArrowLeft':
+        case "ArrowLeft":
             if (movementAt === tick) break;
-            if (direction === 'left' || direction === 'right') break;
-            direction = 'left';
+            if (direction === "left" || direction === "right") break;
+            direction = "left";
             break;
 
-        case 'ArrowRight':
+        case "ArrowRight":
             if (movementAt === tick) break;
-            if (direction === 'left' || direction === 'right') break;
-            direction = 'right';
+            if (direction === "left" || direction === "right") break;
+            direction = "right";
             break;
 
-        case 'Enter':
+        case "Enter":
             if (!gameStarted || gameOver) {
                 startGame();
                 gameStarted = true;
                 break;
             }
 
-        case ' ':
+        case " ":
             if (gameOver) break;
             if (gameStarted) pauseGame();
             break;
